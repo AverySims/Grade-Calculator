b7820ab36ffdf409fa77f4d541e1fa715fa69941
Avery <avery.j.sims@gmail.com>
2023-08-31 23:24:46 -0500

    Update GradeHelper.cs

--- GradeCalculator/GradeCalculator/GradeHelper.cs
+++ GradeCalculator/GradeCalculator/GradeHelper.cs
@@ -2,32 +2,32 @@
 
 public class GradeHelper
 {
-    public static char ConvertGradeToLetter(double grade)
-    {
-        if (grade >= 90)
-        {
-            return 'A';
-        }
-        if (grade >= 80)
-        {
-            return 'B';
-        }
-        if (grade >= 70)
-        {
-            return 'C';
-        }
-        if (grade >= 60)
-        {
-            return 'D';
-        }
-        else
-        {
-            return 'F';
-        }
-    }
-    
-    public static double GetAverageScore(double studentCount, double totalScore)
-    {
-        return totalScore / studentCount;
-    }
+	public static char ConvertGradeToLetter(double grade)
+	{
+		if (grade >= 90)
+		{
+			return 'A';
+		}
+		if (grade >= 80)
+		{
+			return 'B';
+		}
+		if (grade >= 70)
+		{
+			return 'C';
+		}
+		if (grade >= 60)
+		{
+			return 'D';
+		}
+		else
+		{
+			return 'F';
+		}
+	}
+	
+	public static double GetAverageScore(double	studentCount, double totalScore)
+	{
+		return totalScore /	studentCount;
+	}
 }
